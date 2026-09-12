661fd085190b8cc0353f3510100f7583b973c117
ClayWarren <warren.clay@icloud.com>
2026-09-12 18:07:02 -0500

    test: launch JSON-C shell tests through Git sh on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,10 @@
 
-call cmake -G "%CMAKE_GENERATOR%" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release -D LIBRARY_INC=%LIBRARY_INC% .
+call cmake -G "Ninja" -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -D CMAKE_BUILD_TYPE=Release -D LIBRARY_INC=%LIBRARY_INC% .
 if errorlevel 1 exit 1
 
 call cmake --build . --target install --config Release
 if errorlevel 1 exit 1
 
+set "PATH=%LIBRARY_BIN%;%PATH%"
 ctest -C Release --output-on-failure
 if errorlevel 1 exit /b 1
